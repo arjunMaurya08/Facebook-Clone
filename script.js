003fb7c794388f07ccfe25ed30dfd7283f5b13f2
Arjun Maurya <154776886+arjunMaurya08@users.noreply.github.com>
2024-09-24 08:00:59 +0530

    add a dark mode button have to do more work on it to make it more functional

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const loginBtn = document.getElementById('login-btn')
 const userName = document.getElementById('username')
 const password = document.getElementById('password')
+const darkModeBtn = document.getElementById('dark-mode-btn')
 let array = []
 
 loginBtn.addEventListener('click', function() {
@@ -9,3 +10,16 @@ loginBtn.addEventListener('click', function() {
   alert(array)
   array = []
 })
+
+
+darkModeBtn.addEventListener('click', setBgColor)
+
+function setBgColor(){
+  if (document.body.style.backgroundColor === 'white'){
+    document.body.style.backgroundColor = 'black'; 
+    // document.darkModeBtn.style.backgroundColor = 'white';
+    // document.darkModeBtn.style.color = 'white';
+  }else{
+    document.body.style.backgroundColor = 'white'; 
+  }
+}
