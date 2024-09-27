3fcd2d57014c9d9dc8ee3f4a02f2e1e883b2ad32
Arjun Maurya <154776886+arjunMaurya08@users.noreply.github.com>
2024-09-27 08:08:38 +0530

    clear the form after clicking login button and throw error message after filling only one field either username or password

--- script.js
+++ script.js
@@ -3,17 +3,20 @@ const userName = document.getElementById('username')
 const password = document.getElementById('password')
 const darkModeBtn = document.getElementById('dark-mode-btn')
 const forgetPasswordBtn = document.querySelector('.forget-password-btn')
+const createNewAccountBtn = document.getElementById('create-new-account')
 
 
 let array = []
 
 loginBtn.addEventListener('click', function () {
-  if (userName.value.length === 0 && password.value.length === 0) {
+  if (userName.value.length === 0 || password.value.length === 0) {
     alert("Please Enter valid username and passoword!!")
   } else {
     array.push(userName.value)
     array.push(password.value)
     alert(array)
+    userName.value = ""
+    password.value = ""
     array = []
   }
 })
@@ -33,4 +36,9 @@ function setBgColor() {
 
 forgetPasswordBtn.addEventListener('click', function () {
   alert("You Have Clicked Forget Password!!")
+})
+
+
+createNewAccountBtn.addEventListener('click', function(){
+  alert("You are redirecting for creating your account...")
 })
